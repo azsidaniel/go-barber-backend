74f4efac7bd1d75c7bb1c2c3da844e44a3203a6f
Daniel Silva <azsidaniel@gmail.com>
2021-02-07 20:51:16 -0300

    Criando relacionamento de Users com Appointments.

--- src/database/migrations/1612727399796-CreateAppointments.ts
+++ src/database/migrations/1612727399796-CreateAppointments.ts
@@ -8,7 +8,7 @@ export class CreateAppointments1612727399796 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
