ca0ebd1c3ef3070861ffe755004f6c46fd1e8836
Daniel Silva <azsidaniel@gmail.com>
2021-03-02 19:45:51 -0300

    Cria a camada de infra e organiza os imports.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,11 @@ import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 
-import routes from './shared/routes';
-import './shared/database';
-import uploadConfig from './config/upload';
+import routes from './routes';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
-import AppError from './shared/errors/AppError';
+import '@shared/infra/typeorm';
 
 const app = express();
 
