389f69fd85239124f24171c69770a8f4964aa9ff
Daniel Silva <azsidaniel@gmail.com>
2021-03-11 21:07:03 -0300

    Cria testes para ResetPassword.

--- src/modules/users/repositories/fakes/FakeUsersTokenRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersTokenRepository.ts
@@ -1,5 +1,5 @@
 import UserToken from '../../infra/typeorm/entities/UserToken';
-import { v4 } from 'uuid';
+import AppError from '../../../../shared/errors/AppError';
 import IUserTokenRepository from '../IUserTokenRepository';
 import { uuid } from 'uuidv4';
 
@@ -13,9 +13,19 @@ class FakeUsersTokenRepository implements IUserTokenRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      userToken => userToken.token === token,
+    );
 
     return userToken;
   }
---
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -0,0 +1,90 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ResetPasswordService from './ResetPasswordService';
+import FakeUsersTokenRepository from '../repositories/fakes/FakeUsersTokenRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import AppError from '@shared/errors/AppError';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUsersTokenRepository: FakeUsersTokenRepository;
+let resetPassword: ResetPasswordService;
+let fakeHashProvider: FakeHashProvider;
+
+describe('ResetPassword', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeUsersTokenRepository = new FakeUsersTokenRepository();
+    fakeHashProvider = new FakeHashProvider();
+    resetPassword = new ResetPasswordService(
+      fakeUsersRepository,
+      fakeUsersTokenRepository,
+      fakeHashProvider,
+    );
+  });
+
+  it('should be able reset the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const { token } = await fakeUsersTokenRepository.generate(user.id);
+
+    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash');
+
+    await resetPassword.execute({
+      password: 'new-password',
+      token,
+    });
+
+    const updatedUser = await fakeUsersRepository.findById(user.id);
+
+    expect(updatedUser?.password).toBe('new-password');
+    expect(generateHash).toHaveBeenCalledWith('new-password');
+  });
+
+  it('should not be able to reset the password with non-existing token', async () => {
+    await expect(
+      resetPassword.execute({
+        token: 'non-existing-token',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to reset the password with non-existing user', async () => {
+    const { token } = await fakeUsersTokenRepository.generate(
+      'non-existing-user',
+    );
+
+    await expect(
+      resetPassword.execute({
+        token,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to reset the password passed 2 hours after token was created', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const { token } = await fakeUsersTokenRepository.generate(user.id);
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      const customDate = new Date();
+
+      return customDate.setHours(customDate.getHours() + 3);
+    });
+
+    await expect(
+      resetPassword.execute({
+        token,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
